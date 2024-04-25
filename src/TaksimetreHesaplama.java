fe28dfaadc575d5cb4bf7dd78e09223e72bfb2b2
Ali Kurucay <kurucayali@gmail.com>
2024-04-25 09:58:46 +0300

    TaksimetreHesaplama

--- src/TaksimetreHesaplama.java
+++ src/TaksimetreHesaplama.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
 public class TaksimetreHesaplama {
+    public static void main(String[] args) {
+        // değişkenleri tanımla
+        int km;
+        double perkm = 2.20, startprice = 10, total ;
+
+        //km girdisini al
+        Scanner input = new Scanner(System.in);
+        System.out.print("Km değerini giriniz..: ");
+        km = input.nextInt();
+
+        total = startprice + (perkm * km );
+
+        //koşulu oluştur boolean ifade
+        total = (total <20 )? 20 : total ;
+
+        //çıktı
+        System.out.print("Ödenecek tutar..:" + total + "TL");
+
+    }
+
 }
