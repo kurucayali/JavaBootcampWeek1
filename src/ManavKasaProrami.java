8cc69bbb3295b3d633a3d1b4eb332f3325fac8a0
Ali Kurucay <kurucayali@gmail.com>
2024-04-25 14:20:43 +0300

    Degiskenler duzenlendi

--- src/ManavKasaProrami.java
+++ src/ManavKasaProrami.java
@@ -6,24 +6,24 @@ public class ManavKasaProrami {
         //değişkenleri oluşturalım
         //fiyat bazlı sabit değerler
         double armut=2.14, elma=3.67 , domates=1.11, muz= 0.95, patlıcan=5.00;
-        //ürünün ilk harfine atanan değişken kilo miktarına eşitleyerek ilerleyelim
-        double a,e,d,m,p,toplam;
+        //ürünlerin kilo miktarına değişken atayalım
+        double perArmut,perElma,perDomates,perMuz,perPatlıcan,toplam;
 
         //her ürün için inputları alalım
         Scanner input= new Scanner(System.in);
         System.out.print("Armut Kaç Kilo ?..:");
-        a=input.nextDouble();
+        perArmut=input.nextDouble();
         System.out.print("Elma Kaç Kilo ?..:");
-        e=input.nextDouble();
+        perElma=input.nextDouble();
         System.out.print("Domates Kaç Kilo ?..:");
-        d=input.nextDouble();
+        perDomates=input.nextDouble();
         System.out.print("Muz Kaç Kilo ?..:");
-        m=input.nextDouble();
+        perMuz=input.nextDouble();
         System.out.print("Patlıcan Kaç Kilo ?..:");
-        p=input.nextDouble();
+        perPatlıcan=input.nextDouble();
 
         //denklemi kurup toplam fiyatı hesaplayalım
-        toplam= (elma*e)+(armut*a)+(domates*d)+(muz*m)+(patlıcan*p);
+        toplam= (elma*perElma)+(armut*perArmut)+(domates*perDomates)+(muz*perMuz)+(patlıcan*perPatlıcan);
 
         //sonuç çıktısını verelim
 
