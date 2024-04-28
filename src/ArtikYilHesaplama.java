84a697e705991e0f0e49dda15663149c8e51cc09
Ali Kurucay <kurucayali@gmail.com>
2024-04-28 21:55:56 +0300

    v2

--- src/ArtikYilHesaplama.java
+++ src/ArtikYilHesaplama.java
@@ -4,20 +4,24 @@ Artık yıl olup olmadığı bilgisi verilecektir.
  */
 
 import java.util.Scanner;
+
 public class ArtikYilHesaplama {
     public static void main(String[] args) {
+
+        // Değişkeni tanımlıyoruz.
         int year;
+        // Kullanıcıdan yıl bilgisini alıyoruz.
         Scanner input = new Scanner(System.in);
         System.out.print("Yıl giriniz :");
-        year= input.nextInt();
+        year = input.nextInt();
 
-        if (year % 400 == 0 ){
-            System.out.print(year + ( " bir artık yıldır." ));
-        } else if (year %100 != 0 && year %4 ==0 ){
-            System.out.print(year+" bir artık yıldır.");
-        } else if (year < 0 ){
-            System.out.print(" Lütfen M.S. bir tarih için işlem yapınız :) ");
-        }else {
+        //koşulları belirleyip o doğrultu da verilecek çıktıları oluşturuyoruz.
+
+        if (year % 400 == 0) {
+            System.out.print(year + (" bir artık yıldır."));
+        } else if (year % 100 != 0 && year % 4 == 0) {
+            System.out.print(year + " bir artık yıldır.");
+        } else {
             System.out.print(year + " bir artık yıl değildir.");
         }
     }
