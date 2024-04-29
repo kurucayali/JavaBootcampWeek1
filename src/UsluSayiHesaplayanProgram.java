73299cb6e5b0064c72e9d83e62069ea2e28726c5
Ali Kurucay <kurucayali@gmail.com>
2024-04-30 01:28:38 +0300

    yorumlar eklendi

--- src/UsluSayiHesaplayanProgram.java
+++ src/UsluSayiHesaplayanProgram.java
@@ -1,14 +1,24 @@
+//Kullanıcıdan iki sayı alınacaktır. Üssü alınacak sayı ve üssü. Sonra hesaplanığ çıktı verilecektir.
+
 import java.util.Scanner;
 
 public class UsluSayiHesaplayanProgram {
     public static void main(String[] args) {
+
+        // değişkenlerimizi oluşturalım
         int n, k;
+
+        // girdilerimizi alalım
         Scanner input = new Scanner(System.in);
+
         System.out.print("Üssü alınacak sayıyı giriniz.: ");
         n = input.nextInt();
+
         System.out.print("Üs sayıyı giriniz.: ");
         k = input.nextInt();
+
         int result = 1;
+        //Döngüyle üssü kadar çarpma işlemi yaparak sonucu hesaplayalım
         for (int i = 1; i <= k; i++) {
             result *= n;
         }
