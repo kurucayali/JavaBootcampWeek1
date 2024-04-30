ef1c035dba4b1e613867c43d46161a9d8143a20a
Ali Kurucay <kurucayali@gmail.com>
2024-04-30 22:09:09 +0300

    create new file

---
+++ src/BasamakSayilarininToplami.java
@@ -0,0 +1,24 @@
+
+//kullanıcıdan bir değer al ve rakamlarını toplayan program
+import java.util.Scanner;
+
+public class BasamakSayilarininToplami {
+    public static void main(String[] args) {
+
+        //Kullanıcıdan sayı alalım.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = input.nextInt();
+
+        int total = 0;
+        int remainder;
+        //number 0 a eşit olana kadar her kalan bir basamak değerini verecek ve bunlar toplanacak.
+        while (number != 0) {
+            remainder = number % 10;
+            total += remainder;
+            number /= 10;
+        }
+        // Kullanıcıya çıktı verelim
+        System.out.println("Basamaklarının toplamı: " + total);
+    }
+}
