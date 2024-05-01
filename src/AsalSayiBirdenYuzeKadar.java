ebe3e16df5c3516dda92b84cf6cc464d72ce2cfa
Ali Kurucay <kurucayali@gmail.com>
2024-05-01 22:39:31 +0300

    Create AsalSayiBirdenYuzeKadar

--- src/AsalSayiBirdenYuzeKadar.java
+++ src/AsalSayiBirdenYuzeKadar.java
@@ -1,19 +1,21 @@
+//1-100 e kadar olan asal sayıları yazdıran program.
+
 public class AsalSayiBirdenYuzeKadar {
     public static void main(String[] args) {
         // 1'den 100'e kadar olan sayıları kontrol ediyoruz
         for (int i = 2; i <= 100; i++) {
              // Her sayıyı varsayılan olarak asal olarak kabul ediyoruz
-            boolean isPrime = true;
+            boolean asal = true;
             // 2'den sayının kendisine kadar olan tüm sayılarla bölenlerine bakıyoruz
             for (int j = 2; j < i; j++) {
                 // Eğer herhangi bir sayıya tam bölünürse bu sayı asal değildir
                 if (i % j == 0) {
-                    isPrime = false;
+                    asal = false;
                     break;
                 }
             }
             // Eğer sayı asal ise ekrana yazdırıyoruz
-            if (isPrime) {
+            if (asal) {
                 System.out.print(i + " ");
             }
         }
