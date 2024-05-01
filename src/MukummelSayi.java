df1b21435bc0144163727e21657f18b1244e7c2f
Ali Kurucay <kurucayali@gmail.com>
2024-05-01 22:15:00 +0300

    Create MukemmelSayi

---
+++ src/MukummelSayi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class MukummelSayi {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Kullanıcıdan bir sayı girmesini istiyoruz
+        System.out.print("Bir sayı giriniz: ");
+        int number = input.nextInt();
+
+        // Sayının kendisi hariç pozitif tam sayı çarpanları toplamını buluyoruz
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        // Toplamın girilen sayıya eşit olup olmadığını kontrol ediyoruz
+        if (sum == number) {
+            System.out.println(number + " Mükemmel sayıdır.");
+        } else {
+            System.out.println(number + " Mükemmel sayı değildir.");
+        }
+    }
+}
